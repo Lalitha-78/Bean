b8a93ed9e3438037d07fd31d5d6be0b623add397
Lalitha-78 <113717336+Lalitha-78@users.noreply.github.com>
2022-12-21 10:48:02 +0530

    Add files via upload

---
+++ Java Bean/Bean/src/Bean/mainc.java
@@ -0,0 +1,10 @@
+package Bean;
+public class mainc {
+	public static void main(String a[]) {
+	   	 Jbean b= new Jbean();
+	   	 b.setId(95);
+	   	 System.out.println( b.getId());
+	   	 }
+}
+
+
---
+++ Java Bean/Bean/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Lalitha
+ *
+ */
+module Bean {
+}
